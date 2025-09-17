4fd89c3ccd387a8a2c2656053944c610597ac1f9
NimasSeptiandini <septyandini921@gmail.com>
2025-09-17 14:53:46 +0700

    JS3_BMI_Input tinggi dan berat badan

--- jobsheet3/bin/hitungBMI.dart
+++ jobsheet3/bin/hitungBMI.dart
@@ -1,3 +1,11 @@
-main() {
-  
+// bin/main.dart
+void main() {
+  double tinggiCm = 170;
+  double beratKg = 65;
+
+  if (tinggiCm <= 0 || beratKg <= 0) {
+    print("Input tidak valid: tinggi dan berat harus lebih dari 0");
+  } else {
+    print("Input valid. Tinggi: $tinggiCm cm, Berat: $beratKg kg");
+  }
 }
