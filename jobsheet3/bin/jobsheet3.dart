03497f2f607e279817a0b58e62ae308523970a34
NimasSeptiandini <septyandini921@gmail.com>
2025-09-17 14:42:25 +0700

    delete dart JS3(old)

--- jobsheet3/bin/jobsheet3.dart
+++
@@ -1,5 +0,0 @@
-import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${jobsheet3.calculate()}!');
-}
--- jobsheet3/test/jobsheet3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:jobsheet3/jobsheet3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
